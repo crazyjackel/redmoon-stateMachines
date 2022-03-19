f3eedbd8057f3d10ecf0e0925ca389357cfb4203
Jackson Levitt <47047592+crazyjackel@users.noreply.github.com>
2022-03-19 16:14:12 -0400

    Working Version 1.0.0

--- Samples/SwitchCaseSample/SwitchCaseExample.cs
+++ Samples/SwitchCaseSample/SwitchCaseExample.cs
@@ -7,10 +7,22 @@ using Bewildered;
 
 public class SwitchCaseExample : MonoBehaviour
 {
+    public string startingCase;
     public SwitchCaseStateMachine<string> SwitchStateMachine;
+
+    public void Start()
+    {
+        SwitchStateMachine.ProcessCase(startingCase);
+    }
+
 
     public void ProcessCase(string @case)
     {
         SwitchStateMachine.ProcessCase(@case);
+    }
+
+    public void DebugStr(string @debug)
+    {
+        Debug.Log(debug);
     }
 }
